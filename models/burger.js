7560f44460758ed800669264101f257306b36688
Brian Allen <brianallen2401@gmail.com>
2020-04-02 13:11:29 -0600

    finished sequelized burger model. Commented out old ORM model

--- models/burger.js
+++ models/burger.js
@@ -1,30 +1,38 @@
 module.exports = function(sequelize, DataTypes) {
-    let Burger = sequelize.define("Burger", {
-        burger_name: DataTypes.STRING,
-        devoured: DataTypes.BOOLEAN
-    });
-};
-
-
-
-const orm = require('../config/orm.js');
-
-const burger = {
-    selectAll: function(callback) {
-        orm.selectAll("burgers", function(response) {
-            callback(response);
-        });
+  let Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    insertOne: function(columns, values, callback) {
-        orm.insertOne("burgers", columns, values, function(response) {
-            callback(response);
-        });
-    },
-    updateOne: function(objectColumnValues, condition, callback) {
-        orm.updateOne("burgers", objectColumnValues, condition, function(response){
-            callback(response);
-        });
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        defaultValue: false
+      }
     }
+  });
+  return Burger;
 };
 
-module.exports = burger;
+// const orm = require("../config/orm.js");
+
+// const burger = {
+//   selectAll: function(callback) {
+//     orm.selectAll("burgers", function(response) {
+//       callback(response);
+//     });
+//   },
+//   insertOne: function(columns, values, callback) {
+//     orm.insertOne("burgers", columns, values, function(response) {
+//       callback(response);
+//     });
+//   },
+//   updateOne: function(objectColumnValues, condition, callback) {
+//     orm.updateOne("burgers", objectColumnValues, condition, function(response) {
+//       callback(response);
+//     });
+//   }
+// };
+
+// module.exports = burger;
