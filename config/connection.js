24ea839f9b430609b99e715ae5fd854ea32a7470
Brian Allen <brianallen2401@gmail.com>
2020-04-02 14:29:02 -0600

    commented out the connection.js file for the sequelize version

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,24 @@
-const mysql = require("mysql");
-let connection;
+// const mysql = require("mysql");
+// let connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-}
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+//   });
+// }
 
-connection.connect(function(error) {
-  if (error) {
-    console.error("Error connecting: " + error.stack);
-    return;
-  }
-  console.log("Connected as id: " + connection.threadId);
-});
+// connection.connect(function(error) {
+//   if (error) {
+//     console.error("Error connecting: " + error.stack);
+//     return;
+//   }
+//   console.log("Connected as id: " + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
