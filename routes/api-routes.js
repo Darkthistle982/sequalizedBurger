109f463bb187626336c81a184ffad1875a7a3782
Brian Allen <brianallen2401@gmail.com>
2020-04-02 14:28:36 -0600

    added a routes folder to the project

---
+++ routes/api-routes.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    app.get("/", function(request, response) {
+        db.Burger.findAll({}).then(function(result) {
+            return response.render('index', {burgers: result});
+        });
+    });
+
+    app.post("/", function(request, response) {
+        db.Burger.create({
+            burger_name: request.body.burger_name,
+            devoured: request.body.devoured
+        }).then(function() {
+            response.redirect('/');
+        });
+    });
+
+    app.put("/:id", function(request, response) {
+        db.Burger.update(
+            {devoured: 1},
+            {where: {
+                id: request.params.id
+            }}
+        ).then(function(result) {
+            result.redirect('/');
+        });
+    });
+};
