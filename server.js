31f23dd086989732f6a187140a57f2b8b191e8c1
Brian Allen <brianallen2401@gmail.com>
2020-04-02 18:34:32 -0600

    deleted excess spacing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const db = require("./models");
 
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static(process.cwd() + "/views"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/vnd/api+json' }));
@@ -17,7 +17,7 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-require ("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 db.sequelize.sync({force: false}).then(function() {
     app.listen(PORT, function() {
