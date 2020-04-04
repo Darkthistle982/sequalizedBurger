91b4e2bfad36197b37c7316021278f4e7dbc0a4e
Brian Allen <brianallen2401@gmail.com>
2020-04-04 10:09:38 -0600

    deleted console.log used for error tracking. error is now fixed.

--- models/burger.js
+++ models/burger.js
@@ -7,32 +7,8 @@ module.exports = function(sequelize, DataTypes) {
     devoured: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      validate: {
-        defaultValue: false
-      }
+      defaultValue: false
     }
   });
   return Burger;
 };
-
-// const orm = require("../config/orm.js");
-
-// const burger = {
-//   selectAll: function(callback) {
-//     orm.selectAll("burgers", function(response) {
-//       callback(response);
-//     });
-//   },
-//   insertOne: function(columns, values, callback) {
-//     orm.insertOne("burgers", columns, values, function(response) {
-//       callback(response);
-//     });
-//   },
-//   updateOne: function(objectColumnValues, condition, callback) {
-//     orm.updateOne("burgers", objectColumnValues, condition, function(response) {
-//       callback(response);
-//     });
-//   }
-// };
-
-// module.exports = burger;
